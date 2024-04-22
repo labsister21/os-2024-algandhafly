6de3bf4f7911a1a0de51fea05fec77cc8db0235e
AldyDPP <possiblepiano@gmail.com>
2024-04-22 10:47:59 +0700

    fix: fix more logical flaws

--- src/memory/paging.c
+++ src/memory/paging.c
@@ -56,17 +56,17 @@ bool paging_allocate_user_page_frame(struct PageDirectory *page_dir, void *virtu
     uint32_t page_index = ((uint32_t) virtual_addr >> 22) & 0x3FF;
     if (page_dir->table[page_index].flags.present_bit) return false;
 
-    uint32_t page_frame_index = 0;
+    uint16_t page_frame_index = 0;
     for(;;)
     {
-        if (page_frame_index > PAGE_FRAME_MAX_COUNT) return false;
+        if (page_frame_index >= PAGE_FRAME_MAX_COUNT) return false;
         if (!page_manager_state.page_frame_map[page_frame_index]) break;
         else ++page_frame_index;
     }
     
     page_manager_state.page_frame_map[page_frame_index] = 1;
     page_manager_state.free_page_frame_count--;
-    uint32_t physical_addr = page_frame_index;
+    uint16_t physical_addr = page_frame_index;
 
     page_dir->table[page_index].flags.present_bit       = 1;
     page_dir->table[page_index].flags.read_write        = 1;
@@ -85,10 +85,18 @@ bool paging_free_user_page_frame(struct PageDirectory *page_dir, void *virtual_a
     page_dir->table[page_index].flags.present_bit = 0;
     flush_single_tlb(virtual_addr);
     
-    uint32_t physical_addr = page_dir->table[page_index].lower_address;
-    uint32_t page_frame_index = (physical_addr >> 22) & 0x1F;
+    uint32_t page_frame_index = page_dir->table[page_index].lower_address;
     page_manager_state.page_frame_map[page_frame_index] = false;
     page_manager_state.free_page_frame_count++;
 
     return true;
 }
+
+// uint32_t getPhysicalAddress(struct PageDirectory *page_dir, void* virtual_addr) {
+//     uint32_t page_index = ((uint32_t) virtual_addr >> 22) & 0x3FF;
+    
+//     uint32_t page_frame_addr = page_dir->table[page_index].lower_address;
+//     uint32_t offset = (uint32_t) virtual_addr & (0x3FF);
+
+//     return (page_frame_addr << 22) | offset;
+// }
