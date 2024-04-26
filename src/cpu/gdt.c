62d12d65c551443c7b7c37d15adbba5915d95ab8
ganadipa <nyomanganadipa@gmail.com>
2024-04-26 13:28:27 +0700

    Feat: Implemented user mode

--- src/cpu/gdt.c
+++ src/cpu/gdt.c
@@ -104,6 +104,14 @@ struct GlobalDescriptorTable global_descriptor_table = {
     }
 };
 
+void gdt_install_tss(void) {
+    uint32_t base = (uint32_t) &_interrupt_tss_entry;
+    global_descriptor_table.table[5].base_high = (base & (0xFF << 24)) >> 24;
+    global_descriptor_table.table[5].base_mid  = (base & (0xFF << 16)) >> 16;
+    global_descriptor_table.table[5].base_low  = base & 0xFFFF;
+}
+
+
 /**
  * _gdt_gdtr, predefined system GDTR. 
  * GDT pointed by this variable is already set to point global_descriptor_table above.
--- src/kernel.c
+++ src/kernel.c
@@ -10,13 +10,9 @@
 #include "header/driver/keyboard.h"
 #include "header/filesystem/fat32.h"
 #include "header/stdlib/string.h"
+#include "header/kernel-entrypoint.h"
+#include "header/memory/paging.h"
 
-
-/**
- * Keyboard havent handle enter key perfectly:
- * - Not handling edge cases
- * 
-*/
 
 char frame[WIDTH][HEIGHT];
 
@@ -281,6 +277,33 @@ void kernel_setup(void) {
     //     framebuffer_write_length(1, 0, "fat32driver_state.dir_table_buf.table:", 38, White, Black);
     //     framebuffer_write_length(2, 0, fat32driver_state.dir_table_buf.table, CLUSTER_MAP_SIZE*CLUSTER_BLOCK_COUNT, White, Black);
     // }
+
+    load_gdt(&_gdt_gdtr);
+    pic_remap();
+    initialize_idt();
+    activate_keyboard_interrupt();
+    framebuffer_clear();
+    framebuffer_set_cursor(0, 0);
+    initialize_filesystem_fat32();
+    gdt_install_tss();
+    set_tss_register();
+
+    // Allocate first 4 MiB virtual memory
+    paging_allocate_user_page_frame(&_paging_kernel_page_directory, (uint8_t*) 0);
+
+    // Write shell into memory
+    struct FAT32DriverRequest request = {
+        .buf                   = (uint8_t*) 0,
+        .name                  = "shell",
+        .ext                   = "\0\0\0",
+        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+        .buffer_size           = 0x100000,
+    };
+    read(request);
+
+    // Set TSS $esp pointer and jump into shell 
+    set_tss_kernel_current_stack();
+    kernel_execute_user_program((uint8_t*) 0);
 
 
     while (true);
